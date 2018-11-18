81274a9254aa4049b97f350d77c2dceb03d4a21d
Christopher Polito <cpolito343@gmail.com>
2018-11-18 01:36:53 -0500

    update

--- server.js
+++ server.js
@@ -9,12 +9,40 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
-app.use("/", express.static(path.join(__dirname, ".")));
+app.use("/", express.static(path.join(__dirname, "public")));
+
+var PORTS = {
+	heroku:8080,
+	http:80,
+	misc1:3004,
+	misc2:4200,
+	misc3:4210
+}
 
 
+var env = process.env.NODE_ENV;
+var port;
+
+	
+if (process.env.PORT) {
+	port = process.env.PORT;
+}
+else if (env == "production") {
+
+	port = PORTS.heroku;
+
+}
+else if (env == "development") {
+
+	port = PORTS.misc2;
+}
+else {
+
+	port = PORTS.misc1;
+}
 
 
-var listener = app.listen(8080, function () {
+var listener = app.listen(port, function () {
 
 	console.log("listening on port", listener.address().port);
 });
